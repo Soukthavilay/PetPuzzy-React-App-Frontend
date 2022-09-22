a273541e2c7cbc9c27876e6c7ce36932b6189d77
Soukthavilay <Soukthavilay2000@gmail.com>
2022-09-23 00:31:41 +0700

    Create-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">PetPuzzy</a>
       </header>
-      <main>List production</main>
+      <main>
+        <h1>List of products</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>{product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
